09d7c5733901651618da0e93e90b6c9dddd6ad1e
Paul Stearns <celestialcoyote@gmail.com>
2023-02-19 18:41:09 -0800

    Added files from resources download.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,23 @@
-export default () => (
-	<p1>Hello</p1>
-);
+import Card from './Card';
+import styles from './index.module.css';
+
+
+export default () => {
+	return (
+		<div className={styles.app}>
+			<header className={styles.header}>
+				<img
+					src='/logo.png'
+					className={styles.logo} alt='logo'
+				/>
+			</header>
+
+			<div className={styles.grid}>
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		</div>
+	);
+};
 
